c122a185da4ca97e1ead210874649bb3ea51239d
Jake Noga <jacob.noga@hpe.com>
2024-06-20 10:40:51 -0500

    first commit

---
+++ lib.go
@@ -0,0 +1,7 @@
+package workspace_lib
+
+// Create a module in your own public repository. This module has a single function named Add with two int parameters and one int return value.
+// This function adds the two parameters together and returns them. Make this version v1.0.0.
+func Add(var1, var2 int) int {
+	return var1 + var2
+}
